af58db260d260a4cd94e311ab822f0176b947339
Cheloved <email141032@mail.ru>
2018-09-07 20:08:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,65 @@
+#Libraries
 from PIL import Image
 import time 
 
-#xStart = -0.74877
-#xEnd =  -0.74872
+"""
+interesting coordinates
+xStart = -0.74877
+xEnd =  -0.74872
+"""
+#Field of view on OX
 xStart = -3.16
 xEnd = 2.16
 xLen = xEnd - xStart
 
-#yStart = 0.065053
-#yEnd = 0.065103
+"""
+interesting coordinates
+yStart = 0.065053
+yEnd = 0.065103
+"""
+#Field of view on OY
 yStart = -1.5
 yEnd = 1.5
 yLen = yEnd - yStart
 
+#multiplier for auto set field
 multiplier = 10000
 
+#size of output picture
 width = 3840
 height = 2160
 
+#check for manual size
 if width == 0:
     width = int( abs( xLen ) * multiplier )
 
 if height == 0:    
     height = int( abs( yLen ) * multiplier )
 
+#creating image and loading pixels of it
 image = Image.new('RGB', (width, height), (0,0,0))
 pixels = image.load()
 
+#more iterations = high quality
 iterations = 40
 border = 4
 
+#timer for speed test
 startTime = time.time()
 
+#goes all over the image
 for x in range(width):
-    for y in range(height):        
+    for y in range(height):     
+        #making coordinates
         xCurrent = ( ( float(x) * xLen ) / float(width) ) + xStart
         yCurrent = ( ( float(y) * yLen ) / float(height) ) + yStart
         
+        #making complex number
         c = complex(xCurrent, yCurrent)
         z = c
         
+        #this loop is checking if set infinite or not
+        #and changing color of pixels
         for i in range(iterations):
             if abs(z) > border:
                 color = int((i*255)/iterations)
@@ -55,9 +75,11 @@ for x in range(width):
         else:
             pixels[x,y] = (255,255,255)
             
+#information about program perfomance
 delay = time.time() - startTime
 print('Ready in {0:2f} seconds').format(delay)
 
+#showing and saving the image
 image.show()
 image.save('MBSet-'+str(time.time())+'.png')
         
